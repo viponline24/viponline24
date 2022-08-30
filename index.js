d220817c57587a471ceff71d4b8076482028cbdd
viponline24 <112420706+viponline24@users.noreply.github.com>
2022-08-30 17:55:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.write('<h1>viponline24</h1>');
 document.write('Pod Vape Ecig');
 document.write('<br>');
-document.write('https://www.facebook.com/viponline24 ');
+document.write('https://www.facebook.com/viponline24');
 document.write('#vape');
 document.write('#ecig');
 document.write('#pod');
