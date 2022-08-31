70a8dd12f5bf98cc10a5e5205632f33647baabaa
viponline24 <112420706+viponline24@users.noreply.github.com>
2022-08-31 17:30:09 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.write('<h1>viponline24</h1>');
-document.write('Pod Vape Ecig');
-document.write('<br>');
-document.write('https://www.facebook.com/viponline24');
-document.write('#vape');
-document.write('#ecig');
-document.write('#pod');
-document.write('#relx');
